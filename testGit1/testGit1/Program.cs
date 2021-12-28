8b1a90367616601789c8d4e785ec3e13d677546d
maryam <maryamrazi53@gmail.com>
2021-12-28 13:23:46 +0100

    first commit

---
+++ testGit1/testGit1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testGit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello git!");
+            Console.WriteLine("Hello git2!");
+
+
+        }
+    }
+}
